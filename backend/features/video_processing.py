abf65c3e405d16ad49c08580b5491e2dc773196d
Ahmed Elsayed Eltaron <96958864+ahmedeltaron0@users.noreply.github.com>
2024-02-19 16:44:08 +0200

    add video for testing

--- backend/features/video_processing.py
+++ backend/features/video_processing.py
@@ -5,7 +5,7 @@ import os
 separator = Separator('spleeter:2stems')  # '2stems' separates into vocals and accompaniment (music)
 
 # Provide path to your audio file
-audio_file_path = '-----------'
+audio_file_path = 'audio_example_mono.mp3'
 
 # Separate the audio
 separator.separate_to_file(audio_file_path, 'output_folder')
